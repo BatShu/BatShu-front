7cc180a0a38ac8e9eb6567915e415e6ea12aef4b
bluejoyq <bluejoy32@gmail.com>
2023-09-11 17:48:15 +0900

    feat: msw, react query  추가 및 초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import { ReactElement } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // styles
 import { CssBaseline, StyledEngineProvider } from "@mui/material";
 import { GlobalStyles } from "./presentation/common/atoms/GlobalStyles";
 // layout
 import Layout from "./presentation/layout/Layout";
 
+const queryClient = new QueryClient();
+
 export const App = (): ReactElement => {
   return (
-    <StyledEngineProvider injectFirst>
-      <Layout>
-        <RouterProvider router={router} />
-        <CssBaseline />
-        <GlobalStyles />
-      </Layout>
-    </StyledEngineProvider>
+    <QueryClientProvider client={queryClient}>
+      <StyledEngineProvider injectFirst>
+        <Layout>
+          <RouterProvider router={router} />
+          <CssBaseline />
+          <GlobalStyles />
+        </Layout>
+      </StyledEngineProvider>
+    </QueryClientProvider>
   );
 };
