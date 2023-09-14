fc2272243b1db920256ecbf367e259cc706632d3
bluejoyq <bluejoy32@gmail.com>
2023-09-14 18:12:16 +0900

    feat: react query devtools 추가 및 msw 실행

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ReactElement } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { worker } from "./mocks/browser";
 // styles
 import { CssBaseline, StyledEngineProvider } from "@mui/material";
 import { GlobalStyles } from "./presentation/common/atoms/GlobalStyles";
@@ -9,6 +11,10 @@ import { GlobalStyles } from "./presentation/common/atoms/GlobalStyles";
 import Layout from "./presentation/layout/Layout";
 
 const queryClient = new QueryClient();
+
+if (import.meta.env.DEV) {
+  worker.start();
+}
 
 export const App = (): ReactElement => {
   return (
@@ -20,6 +26,7 @@ export const App = (): ReactElement => {
           <GlobalStyles />
         </Layout>
       </StyledEngineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
