c0b657e19f856295474c2af48d29b731c230684c
bluejoyq <bluejoy32@gmail.com>
2023-10-05 22:00:31 +0900

    feat: svgr 설정 및 font 추가, 로고 추가

--- src/presentation/common/styles/font.ts
+++ src/presentation/common/styles/font.ts
@@ -8,3 +8,12 @@ export const font20px500 = css({
   lineHeight: "normal",
   letterSpacing: "-0.41px",
 });
+
+export const font16px600 = css({
+  fontFamily: "Pretendard",
+  fontSize: "16px",
+  fontStyle: "normal",
+  fontWeight: 600,
+  lineHeight: "normal",
+  letterSpacing: "-0.41px",
+});
