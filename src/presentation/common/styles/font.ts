500f4089823ca732af4a0a1a1d35fa7b75cbd85b
bluejoyq <bluejoy32@gmail.com>
2023-10-05 21:37:08 +0900

    feat: 폰트 스타일 추가 및 긴 로고 추가

---
+++ src/presentation/common/styles/font.ts
@@ -0,0 +1,10 @@
+import { css } from "@emotion/react";
+
+export const font20px500 = css({
+  fontFamily: "Pretendard",
+  fontSize: "20px",
+  fontStyle: "normal",
+  fontWeight: 500,
+  lineHeight: "normal",
+  letterSpacing: "-0.41px",
+});
