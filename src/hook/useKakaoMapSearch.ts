e045bb3132c88f94e943f2b423ff0749966e2011
bluejoyq <bluejoy32@gmail.com>
2023-10-05 09:40:13 +0900

    feat: 카카오맵 검색을 useQuery를 사용하도록 개선

--- src/hook/useKakaoMapSearch.ts
+++ src/hook/useKakaoMapSearch.ts
@@ -1,29 +1,22 @@
-import { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 const useKakaoMapSearch = (keyword: string) => {
-  const [result, setResult] = useState<kakao.maps.services.PlacesSearchResult>(
-    []
-  );
-  const [searchError, setSearchError] = useState<boolean>(true);
-
-  useEffect(() => {
-    if (keyword === "") return;
-
-    const placesSearchCB = (
-      data: kakao.maps.services.PlacesSearchResult,
-      status: kakao.maps.services.Status
-    ) => {
-      if (status === kakao.maps.services.Status.OK) {
-        setResult(data);
-        setSearchError(false);
-      } else setSearchError(true);
-    };
-    const ps = new kakao.maps.services.Places();
-
-    ps.keywordSearch(keyword, placesSearchCB);
-  }, [keyword]);
-
-  return { result, searchError };
+  return useQuery({
+    queryKey: ["kakaoMapSearch", keyword],
+    queryFn: async () => {
+      const placesSearchCB = (
+        data: kakao.maps.services.PlacesSearchResult,
+        status: kakao.maps.services.Status
+      ) => {
+        if (status === kakao.maps.services.Status.OK) {
+          return data;
+        }
+        throw new Error("검색 결과가 없습니다.");
+      };
+      const ps = new kakao.maps.services.Places();
+      ps.keywordSearch(keyword, placesSearchCB);
+    },
+  });
 };
 
 export default useKakaoMapSearch;
